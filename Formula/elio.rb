b4c692fe070b4d05e3f80b1aefe1da36bacbd97b
Miguel Pérez Regueiro <67692547+MiguelRegueiro@users.noreply.github.com>
2026-05-22 08:59:59 +0000

    elio: add 1.6.0 bottle.

--- Formula/elio.rb
+++ Formula/elio.rb
@@ -5,6 +5,12 @@ class Elio < Formula
   sha256 "8317d60fd497cff643bbf2ee6a434bcb4e3b084fd5be721cb15d734ad92b324e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/elio-fm/homebrew-elio/releases/download/elio-1.6.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "1feba3d3b1c9c19a926b2518d86d568b4c914a265a6c31bf4b87dead3fb7b247"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4dfd88f64cb97336376d248ad8496b7bad09f7b662ee04cf8035f5931a5723db"
+  end
+
   depends_on "rust" => :build
 
   def install
